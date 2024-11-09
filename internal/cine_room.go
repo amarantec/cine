938c239729921853379702c6849784cbfa5d71f9
Marcos <marcosamarante.c@gmail.com>
2024-11-09 11:41:30 -0300

    improve theater model

--- internal/cine_room.go
+++ internal/cine_room.go
@@ -4,4 +4,5 @@ type CineRoom struct {
 	Id        uint   `json:"id"`
 	Number    string `json:"number"`
 	TheaterId uint   `json:"theater_id"`
+	Seats     []Seat `json:"seats"`
 }
--- internal/theater.go
+++ internal/theater.go
@@ -1,8 +1,7 @@
 package internal
 
 type Theater struct {
-	Id      uint    `json:"id"`
-	Name    string  `json:"name"`
-	Session []Movie `json:"session"`
-	Address string  `json:"address"`
+	Id        uint       `json:"id"`
+	Name      string     `json:"name"`
+	Address   Address    `json:"address"`
 }
