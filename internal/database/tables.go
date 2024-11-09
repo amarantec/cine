d687a4d529e894c4c82e5251f41d56f39a4b9d7b
Marcos <marcosamarante.c@gmail.com>
2024-11-09 17:23:20 -0300

    new tables

--- internal/database/tables.go
+++ internal/database/tables.go
@@ -13,13 +13,50 @@ func createTables(ctx context.Context) {
 			"cast"			TEXT[] NOT NULL,
 			release_date	TIMESTAMP,
 			running_time	INTEGER NOT NULL,
-			age_group		INTEGER NOT NULL
-
+			age_group		INTEGER NOT NULL,
+			created_at		TIMESTAMP DEFAULT NOW(),
+			updated_at		TIMESTAMP DEFAULT NOW(),
+			deleted_at		TIMESTAMP NULL	
 		)`
 
 	_, err := Conn.Exec(ctx, createMovieTable)
 
 	if err != nil {
 		panic("Could not create movies table.")
+	}
+
+	createAddressTable := `
+		CREATE TABLE IF NOT EXISTS address (
+			id			SERIAL PRIMARY KEY,
+			city		TEXT NOT NULL,
+			street		TEXT NOT NULL,
+			zip			TEXT NOT NULL,
+			state		TEXT NOT NULL,
+			country		TEXT NOT NULL,
+			created_at	TIMESTAMP DEFAULT NOW(),
+			updated_at	TIMESTAMP DEFAULT NOW(),
+			deleted_at	TIMESTAMP NULL		
+	)`
+
+	_, err = Conn.Exec(ctx, createAddressTable)
+	
+	if err != nil {
+		panic("Could not create address table.")
+	}
+
+	createTheaterTable := `
+		CREATE TABLE IF NOT EXISTS theaters (
+			id			SERIAL PRIMARY KEY,	
+			name		TEXT NOT NULL UNIQUE,
+			address_id	INTEGER REFERENCES address(id),	
+			created_at	TIMESTAMP DEFAULT NOW(),
+			updated_at	TIMESTAMP DEFAULT NOW(),
+			deleted_at	TIMESTAMP NULL		
+	)`
+
+	_, err = Conn.Exec(ctx, createTheaterTable)
+
+	if err != nil {
+		panic("Could not create theater table.")
 	}
 }
