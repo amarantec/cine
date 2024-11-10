4727d81084462701359b588a233835d1629ca441
Marcos <marcosamarante.c@gmail.com>
2024-11-10 13:31:33 -0300

    address handlers

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"gitlab.com/amarantec/cine/internal/movie"
 	"gitlab.com/amarantec/cine/internal/theater"
+	"gitlab.com/amarantec/cine/internal/address"
 )
 
 func SetRoutes(conn *pgxpool.Pool) *http.ServeMux {
@@ -23,6 +24,12 @@ func SetRoutes(conn *pgxpool.Pool) *http.ServeMux {
 	theaterService := theater.NewTheaterService(theaterRepository)
 	theaterHandler := NewTheaterHandler(theaterService)
 
+	// Address dependency injection
+	addressMux := http.NewServeMux()
+	addressRepository := address.NewAddressRepository(conn)
+	addressService := address.NewAddressService(addressRepository)	
+	addressHandler := NewAddressHandler(addressService)
+
 	/*
 		ROUTES
 	*/
@@ -37,7 +44,15 @@ func SetRoutes(conn *pgxpool.Pool) *http.ServeMux {
 	theaterMux.HandleFunc("/get-theater-by-id/{id}", theaterHandler.getTheaterById)
 	theaterMux.HandleFunc("/add-theater", theaterHandler.addTheater)
 
+	addressMux.HandleFunc("/insert-address", addressHandler.insertAddress)
+	addressMux.HandleFunc("/get-address/{id}", addressHandler.getAddress)
+	addressMux.HandleFunc("/update-address", addressHandler.updateAddress)
+	addressMux.HandleFunc("/delete-address/{id}", addressHandler.deleteAddress)
+	
+ 
     m.Handle("/movies/", http.StripPrefix("/movies", movieMux))
     m.Handle("/theaters/", http.StripPrefix("/theaters", theaterMux))
+	m.Handle("/address/", http.StripPrefix("/address", addressMux))
+
 	return m
 }
