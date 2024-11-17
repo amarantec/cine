7300ff6e064b0e4bcdb000ac8b18a334e0965cff
amarantec <marcosamarante.c@gmail.com>
2024-11-17 19:44:24 -0300

    add time log

--- internal/cine_room.go
+++ internal/cine_room.go
@@ -1,8 +1,13 @@
 package internal
 
+import "time"
+
 type CineRoom struct {
-	Id        uint   `json:"id"`
-	Number    string `json:"number"`
-	TheaterId uint   `json:"theater_id"`
-	Seats     []Seat `json:"seats"`
+	Id        uint       `json:"id"`
+	Number    string     `json:"number"`
+	TheaterId uint       `json:"theater_id"`
+	Seats     []Seat     `json:"seats"`
+    CreatedAt time.Time  `json:"created_at"`
+    UpdatedAt *time.Time `json:"updated_at"`
+    DeletedAt *time.Time `json:"deleted_at"`
 }
