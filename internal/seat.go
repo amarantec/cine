16c10160bcdfb330c593eb7ee0a1f4d420533626
amarantec <marcosamarante.c@gmail.com>
2024-11-17 19:45:44 -0300

    add time log

--- internal/seat.go
+++ internal/seat.go
@@ -1,7 +1,13 @@
 package internal
 
+import "time"
+
 type Seat struct {
-	Row         byte   `json:"row"`
-	PlaceNumber uint   `json:"place_number"`
-	CineRoom    string `json:"cine_room"`
+	Row         byte        `json:"row"`
+	PlaceNumber uint        `json:"place_number"`
+	CineRoom    string      `json:"cine_room"`
+    Available   bool        `json:"available"`
+    CreatedAt   time.Time   `json:"created_at"`
+    UpdatedAt   *time.Time  `json:"updated_at"`
+    DeletedAt   *time.Time  `json:"deleted_at"`
 }
