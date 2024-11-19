839559104c2ab8687722a70ac680a1f153a44a15
amarantec <marcosamarante.c@gmail.com>
2024-11-18 23:09:59 -0300

    simplify mux routers

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -8,31 +8,29 @@ import (
 	"gitlab.com/amarantec/cine/internal/theater"
 	"gitlab.com/amarantec/cine/internal/address"
 	"gitlab.com/amarantec/cine/internal/room"
+     httpSwagger "github.com/swaggo/http-swagger"
+     _ "gitlab.com/amarantec/cine/docs/swagger"
 )
 
 func SetRoutes(conn *pgxpool.Pool) *http.ServeMux {
 	m := http.NewServeMux()
 
 	// Movie dependency injection
-    movieMux := http.NewServeMux()
 	movieRepository := movie.NewMovieRepository(conn)
 	movieService := movie.MovieService(movieRepository)
 	movieHandler := NewMovieHandler(movieService)
 
 	// Theater dependency injection
-    theaterMux := http.NewServeMux()
 	theaterRepository := theater.NewTheaterRepository(conn)
 	theaterService := theater.NewTheaterService(theaterRepository)
 	theaterHandler := NewTheaterHandler(theaterService)
 
 	// Address dependency injection
-	addressMux := http.NewServeMux()
 	addressRepository := address.NewAddressRepository(conn)
 	addressService := address.NewAddressService(addressRepository)	
 	addressHandler := NewAddressHandler(addressService)
 
 	// Cine Room dependency injection
-	roomMux := http.NewServeMux()
 	roomRepository := room.NewRoomRepository(conn)
 	roomService := room.NewRoomService(roomRepository)
 	roomHandler := NewRoomHandler(roomService)
@@ -41,30 +39,28 @@ func SetRoutes(conn *pgxpool.Pool) *http.ServeMux {
 		ROUTES
 	*/
 
-	movieMux.HandleFunc("/list-movies", movieHandler.listMovies)
-	movieMux.HandleFunc("/get-movie-by-id/{id}", movieHandler.getMovieById)
-	movieMux.HandleFunc("/add-movie", movieHandler.addMovie)
-	movieMux.HandleFunc("/get-movies-by-genre/{genre}", movieHandler.getMoviesByGenre)
-    movieMux.HandleFunc("/update-movie", movieHandler.updateMovie)
-    movieMux.HandleFunc("/delete-movie/{movieId}", movieHandler.deleteMovie)
+	m.HandleFunc("/list-movies", movieHandler.listMovies)
+	m.HandleFunc("/get-movie-by-id/{id}", movieHandler.getMovieById)
+	m.HandleFunc("/add-movie", movieHandler.addMovie)
+	m.HandleFunc("/get-movies-by-genre/{genre}", movieHandler.getMoviesByGenre)
+    m.HandleFunc("/update-movie", movieHandler.updateMovie)
+    m.HandleFunc("/delete-movie/{movieId}", movieHandler.deleteMovie)
 
-	theaterMux.HandleFunc("/list-theaters", theaterHandler.listTheaters)
-	theaterMux.HandleFunc("/get-theater-by-id/{id}", theaterHandler.getTheaterById)
-	theaterMux.HandleFunc("/add-theater", theaterHandler.addTheater)
+	m.HandleFunc("/list-theaters", theaterHandler.listTheaters)
+	m.HandleFunc("/get-theater-by-id/{id}", theaterHandler.getTheaterById)
+	m.HandleFunc("/add-theater", theaterHandler.addTheater)
 
-	addressMux.HandleFunc("/insert-address", addressHandler.insertAddress)
-	addressMux.HandleFunc("/get-address/{id}", addressHandler.getAddress)
-	addressMux.HandleFunc("/update-address", addressHandler.updateAddress)
-	addressMux.HandleFunc("/delete-address/{id}", addressHandler.deleteAddress)
+	m.HandleFunc("/insert-address", addressHandler.insertAddress)
+	m.HandleFunc("/get-address/{id}", addressHandler.getAddress)
+	m.HandleFunc("/update-address", addressHandler.updateAddress)
+	m.HandleFunc("/delete-address/{id}", addressHandler.deleteAddress)
 	
-	roomMux.HandleFunc("/list-room", roomHandler.listRooms)
-	roomMux.HandleFunc("/get-room-by-id/{theaterId}", roomHandler.getRoomById)
-	roomMux.HandleFunc("/list-available-room-seats/{theaterId}/{roomNumber}", roomHandler.listAvailableRoomSeats)
+	m.HandleFunc("/list-room", roomHandler.listRooms)
+	m.HandleFunc("/get-room-by-id/{theaterId}", roomHandler.getRoomById)
+	m.HandleFunc("/list-available-room-seats/{theaterId}/{roomNumber}", roomHandler.listAvailableRoomSeats)
  
-    m.Handle("/movies/", http.StripPrefix("/movies", movieMux))
-    m.Handle("/theaters/", http.StripPrefix("/theaters", theaterMux))
-	m.Handle("/address/", http.StripPrefix("/address", addressMux))
-	m.Handle("/room/", http.StripPrefix("/room", roomMux))
 
+
+    m.HandleFunc("/swagger/", httpSwagger.WrapHandler) // Access the Swagger UI
 	return m
 }
